6b8d10e306306529398a30073e7e62b9ca93069d
Kohantika Nath <kohan.nath@gmail.com>
2025-02-21 17:55:27 +0530

    feat: Application Layer Caching using Read Cache(Task 3/5)

--- app/services/visit_counter.py
+++ app/services/visit_counter.py
@@ -1,12 +1,17 @@
 from typing import Dict, List, Any
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 from ..core.redis_manager import RedisManager
 from collections import defaultdict
 
 class VisitCounterService:
     # visitCounter = defaultdict(int)
     # visitCounterLocks = defaultdict(asyncio.Lock)
+
+    # Cache for storing visit count for a page
+    cache_visit_count: Dict[str, Dict] = {}
+    cache_locks = defaultdict(asyncio.Lock)
+    ttl = 50
     def __init__(self):
         """Initialize the visit counter service with Redis manager"""
         self.redis_manager = RedisManager()
@@ -37,6 +42,22 @@ class VisitCounterService:
         # async with VisitCounterService.visitCounterLocks[page_id]:
         #     return VisitCounterService.visitCounter[page_id]
 
-        counts = await self.redis_manager.get(page_id)
-        # TODO: Implement getting visit count
-        return counts
+
+        # 
+        if page_id in VisitCounterService.cache_visit_count:
+            if datetime.now() - VisitCounterService.cache_visit_count[page_id]['time'] < timedelta(seconds=VisitCounterService.ttl):
+
+                async with VisitCounterService.cache_locks[page_id]:
+                    return VisitCounterService.cache_visit_count[page_id]['count']
+
+
+        visit_count = await self.redis_manager.get(page_id)
+        if visit_count is None:
+            return 0
+
+
+        # update in-memory cache
+        async with VisitCounterService.cache_locks[page_id]:
+            VisitCounterService.cache_visit_count[page_id] = {'count': visit_count, 'time': datetime.now()}
+            return visit_count
+                
